005fe0697dcd0af3e92ec1e400c48cb18da1a6e8
BT_1E10_24 <sample@bths.edu>
2022-02-11 13:25:52 -0500

    added solution change

--- src/GroceryStore.java
+++ src/GroceryStore.java
@@ -18,6 +18,15 @@ public class GroceryStore
     public ArrayList<Integer> getReorderList(int min)
     {
         /* to be implemented in part (a) */
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        for (int i = 0; i < productsStocked.length; i++)
+        {
+            if (productsStocked[i].getQuantity() <= min)
+            {
+                indices.add(i);
+            }
+        }
+        return indices;
     }
 
     /** Returns true if all products named in shoppingList are available for purchase
@@ -28,5 +37,35 @@ public class GroceryStore
     public boolean checkAvailability(ArrayList<String> shoppingList)
     {
         /* to be implemented in part (b) */
+        for (String item : shoppingList)
+        {
+            for (Product product : productsStocked)
+            {
+                if (item.equals(product.getName()))
+                {
+                    if (product.getQuantity() < 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /** Returns an ArrayList containing all Products from productStocked that
+     *  have a weight that exceeds the “weight” value provided in the parameter
+     */
+    public ArrayList<Product> oversized(double weight)
+    {
+        ArrayList<Product> overSizedProducts = new ArrayList<Product>();
+        for (Product product : productsStocked)
+        {
+            if (product.getWeight() > weight)
+            {
+                overSizedProducts.add(product);
+            }
+        }
+        return overSizedProducts;
     }
 }
